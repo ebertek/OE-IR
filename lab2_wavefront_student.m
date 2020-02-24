615e683ab307c90f17bea69b70a1c84ef13dd502
David Ebert <ebertek@gmail.com>
2020-02-24 20:35:45 +0100

    Update lab2_wavefront_student.m

--- lab2_wavefront_student.m
+++ lab2_wavefront_student.m
@@ -1,37 +1,37 @@
-%Tested on Matlab R2016b Academic
+%Tested on MATLAB R2019b Academic
 
 clear all;
 close all;
 
 %% Basemaps
-map=imread('lab2_32.png'); %N 32x32
+map = imread('lab2_32.png'); %N 32x32
 
 
 %% "defines"
-emptyVal=0;
-finishVal=-1;
-startVal=-2;
-obstacleVal=-3;
+emptyVal    =  0;
+finishVal   = -1;
+startVal    = -2;
+obstacleVal = -3;
 
 
 %% get start and finish positions
 %find R
-startPos=[-1,-1]; %invalid
-for i=1:size(map,1)
-    for j=1:size(map,2)
-        if reshape(map(i,j,:),[1 3])==[255,0,0] %red
-            startPos=[i,j];
+startPos = [-1,-1]; %invalid
+for i = 1:size(map,1)
+    for j = 1:size(map,2)
+        if reshape(map(i,j,:),[1 3]) == [255,0,0] %red
+            startPos = [i,j];
         end
     end
 end
 
 
 %find F
-finishPos=[-1,-1]; %invalid
-for i=1:size(map,1)
-    for j=1:size(map,2)
-        if reshape(map(i,j,:),[1 3])==[0,255,0] %green
-            finishPos=[i,j];
+finishPos = [-1,-1]; %invalid
+for i = 1:size(map,1)
+    for j = 1:size(map,2)
+        if reshape(map(i,j,:),[1 3]) == [0,255,0] %green
+            finishPos = [i,j];
         end
     end
 end
@@ -40,24 +40,24 @@ disp(['start coords: ', num2str(startPos)]);
 disp(['finish coords: ', num2str(finishPos)]);
 
 %% Replace start, finish, obstacle cell values
-map=rgb2gray(map);
-map=1-im2double(map);
-map(finishPos(1), finishPos(2))=finishVal; %finish
-map(startPos(1), startPos(2))=startVal; %start
-for i=1:size(map,1)
-    for j=1:size(map,2)
-        if map(i,j)==1
-            map(i,j)=obstacleVal; %obstacle
+map = rgb2gray(map);
+map = 1 - im2double(map);
+map(finishPos(1), finishPos(2)) = finishVal; %finish
+map(startPos(1), startPos(2)) = startVal; %start
+for i = 1:size(map,1)
+    for j = 1:size(map,2)
+        if map(i,j) == 1
+            map(i,j) = obstacleVal; %obstacle
         end
     end
 end
 
 %% generate_wavefront
 
-newWavefrontCells=1;
-while(newWavefrontCells>0)
-      newWavefrontCells=0;
-      cloneMap=map;
+newWavefrontCells = 1;
+while newWavefrontCells > 0
+      newWavefrontCells = 0;
+      cloneMap = map;
 % 1. feladat
     % Hullámfrontok terjesztése
     % Addig fut a ciklus, amíg van olyan üres cella, ahova a hullám
@@ -76,12 +76,12 @@ while(newWavefrontCells>0)
 
     %%% IDE TESSÉK ÍRNI
 
-    for i=1:size(map,1)
-        for j=1:size(map,2)
+    for i = 1:size(map,1)
+        for j = 1:size(map,2)
             if map(i,j) == 0 % üres cella
                 ujertek = intmax;
-                for x=-1:1
-                    for y=-1:1
+                for x = -1:1
+                    for y = -1:1
                         ertek = intmax;
                         if map(i+x,j+y) > 0
                             ertek = map(i+x,j+y) + 1 + abs(x) + abs(y);
@@ -107,7 +107,7 @@ while(newWavefrontCells>0)
 
 
 
-    map=cloneMap;
+    map = cloneMap;
     imagesc(map);
     colormap(jet);
     colorbar;
@@ -116,30 +116,30 @@ end
 
 
 %% check wavefront vs. startPos
-localMax=-1;
-for i=-1:1
-    for j=-1:1
-        if map(startPos(1)+i,startPos(2)+j)>0
-           localMax=map(startPos(1)+i,startPos(2)+j);
+localMax = -1;
+for i = -1:1
+    for j = -1:1
+        if map(startPos(1)+i,startPos(2)+j) > 0
+           localMax = map(startPos(1)+i,startPos(2)+j);
         end
     end
 end
-if localMax>0
-    pathavailable=1;
+if localMax > 0
+    pathavailable = 1;
     disp('There is a path!');
 else
-    pathavailable=0;
+    pathavailable = 0;
     disp('There is NO path!');
 end
 
 %% search for the shortest path
 if pathavailable
-    globalMax=max(map(:));
-    if localMax>0
-        pathcomplete=0;
-        robotPos(1)=startPos(1);
-        robotPos(2)=startPos(2);
-        while(~pathcomplete)
+    globalMax = max(map(:));
+    if localMax > 0
+        pathcomplete = 0;
+        robotPos(1) = startPos(1);
+        robotPos(2) = startPos(2);
+        while ~pathcomplete
 
 % 2. feladat
     % Legrövidebb út keresése
@@ -150,8 +150,8 @@ if pathavailable
     %%% IDE TESSÉK ÍRNI
 
     legkisebb = intmax;
-    for x=-1:1
-        for y=-1:1
+    for x = -1:1
+        for y = -1:1
             if map(robotPos(1)+x,robotPos(2)+y) > 0 && map(robotPos(1)+x,robotPos(2)+y) < legkisebb
                 legkisebb = map(robotPos(1)+x,robotPos(2)+y);
                 minWave = [robotPos(1)+x,robotPos(2)+y];
@@ -168,8 +168,8 @@ if pathavailable
 
 
 
-            robotPos=minWave;
-            map(robotPos(1), robotPos(2))=globalMax*1.2; %draw path
+            robotPos = minWave;
+            map(robotPos(1), robotPos(2)) = globalMax * 1.2; %draw path
             %% show the wavefronts
             imagesc(map);
             colormap(jet);
