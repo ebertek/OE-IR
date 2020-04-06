0408bc842cb360ea38690ed97a1202848fe5abc6
David Ebert <ebertek@gmail.com>
2020-04-06 16:59:58 +0200

    Create lab7_10.m

---
+++ lab7_10.m
@@ -0,0 +1,48 @@
+close all;
+clear all;
+warning('off','all');
+
+tic
+%keresett fekete-fehér kép:
+GA_TARGET = double(imresize(im2bw(imread('lab5_happy02.png'),0.5),[64 64]));
+GA_POP_SIZE=1000; %generáció méret
+GA_MAX_ITER=500; %max iteráció
+GA_ELITE_RATE=0.1; %elitráta
+GA_MUTATION_RATE=0.0005; %mutációs ráta
+
+Generation=0;
+%kezdeti egyedek:
+Population=randi([0 1], length(GA_TARGET),length(GA_TARGET), GA_POP_SIZE);
+while Generation<GA_MAX_ITER
+    %fitness számítás
+    Difference=abs(Population-GA_TARGET);
+    Fitness=sum(sum(Difference));
+    %fitness szerinti rendezés
+    [Fitness, index]=sort(Fitness);
+    Population=Population(:,:,index);
+    %legjobb egyed:
+    disp(sprintf('%d. generáció legjobb jósága: %d', Generation, Fitness(1)));
+    imshow((Population(:,:,1)),[]);
+    
+    if Fitness(1)==0
+        break;
+    end;
+    
+    %elitek kiválasztása:
+    EliteSelection=Population(:,:,1:floor(GA_ELITE_RATE * GA_POP_SIZE));
+    %keresztezés
+    for i=1:GA_POP_SIZE
+        Parent1=randi([1 floor(GA_ELITE_RATE * GA_POP_SIZE)]); %egyik szülõ
+        Parent2=randi([1 floor(GA_ELITE_RATE * GA_POP_SIZE)]); %másik szülõ
+        CrossoverPoint=randi([0 1],length(GA_TARGET),length(GA_TARGET)); %véletlen keresztezés
+        Population(:,:,i)=EliteSelection(:,:,Parent1).*CrossoverPoint + EliteSelection(:,:,Parent2).*(1-CrossoverPoint);
+    end
+    %véletlen mutáció, gén=pixel
+    for i=1:length(Population(:))*GA_MUTATION_RATE
+        Population(randi([1 length(GA_TARGET)]), randi([1 length(GA_TARGET)]), randi([1 GA_POP_SIZE]))= randi([0 1]);
+    end
+    
+    Generation=Generation+1;
+end %while vége
+toc
+    
